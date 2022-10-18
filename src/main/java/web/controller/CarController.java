bd3ae71534a10c551ae28b32aeb7e9edaceec348
ggloteneria <8937moto>
2022-10-18 11:56:12 +0400

    fixed task

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,17 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
-    @Autowired
-    CarServiceImpl carServiceImpl;
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping("/cars")
     public String printCars(ModelMap model, @RequestParam(defaultValue = "5") int count) {
-        model.addAttribute("cars", carServiceImpl.getFixNumberOfCars(count));
+        model.addAttribute("cars", carService.getFixNumberOfCars(count));
         return "cars";
     }
 
